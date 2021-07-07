e87e8d9e944c5bb4b6686b1f875ef1c448ce30b8
Liuda Arkavenko <arkavenko18@gmail.com>
2021-07-07 11:51:21 +0300

    finall commit

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,13 @@ $(function () {
     $('.rightside-menu__close').on('click',function(){
         $('.rightside-menu').addClass('rightside-menu--close')
     })
+    $('.header__btn-menu').on('click', function(){
+        $('.menu').toggleClass('menu--open')
+    })
+    if($(window).width() < 651){
+        $('.works-path__item--measurements').appendTo($('.works-path__items-box'));
+        console.log("hi")
+    }
     $('.top__slider').slick({
         dots:true,
         arrows:false,
@@ -15,7 +22,58 @@ $(function () {
         slidesToShow: 10,
         slidesToScroll: 10,
         dots:true,
-        arrows:false
+        arrows:false,
+        responsive:[
+            {
+                breakpoint:1700,
+                settings:{
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                }
+            },
+            {
+                breakpoint:1511,
+                settings:{
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                }
+            },
+            {
+                breakpoint:1300,
+                settings:{
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                }
+            },
+            {
+                breakpoint:1140,
+                settings:{
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                }
+            },
+            {
+                breakpoint:920,
+                settings:{
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint:640,
+                settings:{
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint:440,
+                settings:{
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+        ]
     });
     $('.article-slider__box').slick({
         prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="arrow"></button>',
